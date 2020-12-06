3f2cfde5464cc1ad2aa371f84eb2170776626a8c
Patricia Gomes <patriciagomesro33@gmail.com>
2020-12-05 22:22:21 -0400

    Arquivo de configuração do banco de dados

---
+++ Core/Config.php
@@ -0,0 +1,9 @@
+<?php 
+class Config {
+	const HOST = "localhost";
+	const USER = "root";
+	const PASS = "";
+	const DBNAME = "controle_de_estoque";
+
+	const CHARSET = "utf8";//Padrao de caracteres
+}
