fe706e4bf00da7b9e92bd509e919765d7bee532e
Patricia Gomes <patriciagomesro33@gmail.com>
2021-01-05 22:04:54 -0400

    Inserindo estoque na tabela entry

--- Controllers/stockController.php
+++ Controllers/stockController.php
@@ -3,8 +3,71 @@ class stockController extends Controller {
 
 
 	public function index() {
-		$dados['name_title'] = "Supplier | Controle de Estoque";
+		$model = new Model();
+		$info_product_with_id = array();
+		$info_product = array();
+		$info_supplier = array();
+
+		//Se o id do produto for enviado, seleciona pelo id
+		if(!empty($_POST['id'])) {
+			$info_product_with_id = $model->Select_With_Where('products', array('id'=>$_POST['id']));
+		} else {
+			$info_product = $model->Select_All('products');
+		}
+		$info_supplier = $model->Select_All('supplier');
+
+		$dados['name_title'] = "Stock | Controle de Estoque";
+		$dados['info_product'] = $info_product;
+		$dados['info_supplier'] = $info_supplier;
+		$dados['info_product_with_id'] = $info_product_with_id;
 
 		$this->load_template('insert_stock', $dados);
 	}
+
+	public function insert() {
+		$model = new Model();
+
+		if(!empty($_POST['id_supplier']) && !empty($_POST['id_product']) && !empty($_POST['quant']) && !empty($_POST['value_product']) && !empty($_POST['value_total'])) {
+			$id_supplier = $_POST['id_supplier'];
+			$id_product = $_POST['id_product'];
+			$quant = $_POST['quant'];
+			$value_product = str_replace(',', '.', $_POST['value_product']);
+			$total = str_replace("R$", "", $_POST['value_total']);
+			$value_total =  str_replace(',', '.', $total);
+			date_default_timezone_set('America/Porto_Velho');
+		//	echo "Value total: ".$value_total;
+
+			if(!empty($_POST['expirion_date'])) {
+				$expirion_date = $_POST['expirion_date'];
+			} else {
+				$expirion_date = "NULL";
+			}
+
+			$name_product = $model->Select_With_Where('products', array('id'=>$id_product));
+				foreach($name_product as $value) {
+					$name_product = $value['name'];
+				}
+			
+			$name_supplier = $model->Select_With_Where('supplier', array('id'=>$id_supplier));
+				foreach($name_supplier as $value) {
+					$name_supplier = $value['name'];
+				}
+
+			$model->Insert('entry', array(
+				'name_product'=> $name_product,
+				'name_supplier'=> $name_supplier,
+				'value_product'=> $value_product,
+				'quant_product'=> $quant,
+				'value_total'=> $value_total,
+				'expirion_date'=> $expirion_date,
+				'id_supplier'=> $id_supplier,
+				'id_product'=> $id_product,
+				'date_time' => date("Y-m-d H:i:s")
+			));
+			header('Location: '.BASE_URL.'/entry');
+		} else {
+			echo "Algum campo não foi preenchido!";
+		}
+	}
+
 }
--- Views/insert_stock.php
+++ Views/insert_stock.php
@@ -1,8 +1,7 @@
 			<div class="col-10 content">
 				<!--- Row form busca e menu button--->
 				<div class="row serach_button">
-					<div class="col-4">
-						
+					<div class="col-4">						
 						<form>
 							<div class="input-group">
 							    <input type="search" class="form-control" placeholder="Pesquisar...">
@@ -19,33 +18,75 @@
 				<!-- Row form insert -->
 				<div class="row panel">
 					<div class="col-6">
-						<form>
+						<h2>Inserir no estoque</h2>
+						<form method="POST" action="<?php echo BASE_URL; ?>/stock/insert">	
+							<?php if(!empty($info_supplier)): ?>
+								<div class="form-row">
+									 <div class="form-group col-md-5">
+										<label for="exampleFormControlInput1">Fornecedor
+										</label>
+										<select class="form-control" name="id_supplier" id="exampleFormControlSelect1" required="required">
+											<option></option>
+										<?php foreach($info_supplier as $index => $supplier): ?>
+											<option value="<?php echo $supplier['id']; ?>"><?php echo $supplier['name']; ?></option>
+										<?php endforeach; ?>
+										</select>
+									</div>
+								</div>
+							<?php endif; ?>
+					<!------------------>
+							<?php if(!empty($info_product_with_id)): ?>
+								<div class="form-row">
+								    <div class="form-group col-md-5">
+								    	<?php foreach($info_product_with_id as $info_product): ?>
+										<label for="exampleFormControlInput1">Produto</label>
+										<input type="text" name="id_product" class="form-control"
+										value="<?php echo $info_product['name']; ?>" required="required">
+										<input type="hidden" name="id_product" value="<?php echo $info_product['id']; ?>">
+										<?php endforeach; ?>
+								    </div>
+								</div>
+							<?php else: ?>
+								<div class="form-row">
+									<div class="form-group col-md-5">
+										<label for="exampleFormControlInput1">Produto</label>
+										<select class="form-control" name="id_product" id="exampleFormControlSelect1" required="required">
+												<option></option>
+											<?php foreach($info_product as $product): ?>
+												<option value="<?php echo $product['id']; ?>">
+													<?php echo $product['name']; ?></option>
+											<?php endforeach; ?>
+										</select>
+									</div>
+								</div>
+							<?php endif; ?>
 							<div class="form-row">
-								<div class="form-group col-md-6">
-									<label for="exampleFormControlInput1">Fornecedor do produto</label>
-									<input type="text" class="form-control" id="exampleFormControlInput1" required="required">
-							    </div>
-							</div>
-							<div class="form-row">
-								<div class="form-group col-md-6">
-									<label for="exampleFormControlInput1">Produto</label>
-									<input type="text" class="form-control" id="exampleFormControlInput1" required="required">
+								<div class="form-group col-md-4">
+									<label for="exampleFormControlInput1">Data de validade</label>
+									<input type="date" class="form-control" name="expirion_date">
 							    </div>
 							</div>
 							<div class="form-row">
 							    <div class="form-group col-md-2">
 									<label for="exampleFormControlInput1">Quantidade</label>
-									<input type="number" class="form-control" id="exampleFormControlInput1" required="required">
+									<input type="number" name="quant" class="form-control" id="quant" required="required">
 							    </div>
 							    <div class="form-group col-md-2">
-								    <label for="exampleFormControlSelect1">Preço</label>
-								    <input type="text" class="form-control" id="exampleFormControlInput1" required="required">
+							    	<?php if(!empty($info_product_with_id)): ?>
+								    	<?php foreach($info_product_with_id as $info_product): ?>
+											<label for="exampleFormControlSelect1">Preço</label>
+												<input type="text" name="value_product" value="<?php echo str_replace('.', ',', $info_product['value_medium']); ?>" class="form-control" id="value" required="required">
+										<?php endforeach; ?>
+									<?php else: ?>
+										<label for="exampleFormControlSelect1">Preço</label>
+											<input type="text" name="value_product" class="form-control" id="value" required="required">
+									<?php endif; ?>
 								</div>
 							</div>
-							<div class="form-row">
-							    <div class="form-group col-md-2">
+							<div class="form-row">					
+							    <div class="form-group col-md-3">
 									<label for="exampleFormControlInput1">Valor total</label>
-									<input type="text" class="form-control" id="exampleFormControlInput1" required="required">
+									<input type="text" name="value_total" class="form-control" id="result" required="required">
 							    </div>
 							</div>
 							<button type="submit" class="btn btn-primary">Inserir no estoque</button>
